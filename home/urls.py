848ae53aece603040d6f6e3b732a3f59e812f12e
SamMartin92 <samm92@live.ie>
2023-01-23 10:42:06 +0000

    fixes expenses template issue

--- home/urls.py
+++ home/urls.py
@@ -4,13 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('primary/', views.primary, name='primary'),
-    path('secondary/', views.secondary, name='secondary'),
+    path('view_expenses/', views.view_expenses, name='view_expenses'),
     path('others/', views.others, name='others'),
     path('news/', views.news, name='news'),
     path('financedata/', views.financedata, name='finance'),
     path('add_expense', views.add_expense, name="add_expense"),
     path('edit_expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
-    path('expense/', views.index_expense, name="index_expense")
+    # path('expense/', views.index_expense, name="index_expense")
     
 ]
